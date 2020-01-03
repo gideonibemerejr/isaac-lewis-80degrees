ae0811331afb7367f5a5870a82c9f939e7474462
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2020-01-03 00:27:17 -0600

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-
+import { BrowswerRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 
 const App = () => {
   return (
-
+  <Router>
+    <Route exact path="/"/>
+    <Route/>
+    <Route/>
+    <Route/>
+  </Router>
   )
 }
 
