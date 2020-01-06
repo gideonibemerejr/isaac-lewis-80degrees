75e44ec1e5af552148b0cae08e0df47d617b8370
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2020-01-05 18:42:39 -0600

    Refactored App component to class component

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import './App.css';
 import { ContactPage, HomePage } from "./pages";
 
-const App = () => {
-  return (
-  <Router>
-    <div className="sans-serif ma3">
-      <nav className='red flex justify-between items-center'>
-        <div className="link">
-          <Link to="/" className="link"><h2 className='f2 ma0'>Issac Lewis</h2></Link>
-        </div>
-        <ul className="pa0 flex justify-around list">
+export default class App extends Component {
+  render() {
+    return (
+        <Router>
+          <div className="sans-serif ma3">
+            <nav className='red flex justify-between items-center'>
+              <div className="link">
+                <Link to="/" className="link"><h2 className='f2 ma0'>Issac Lewis</h2></Link>
+              </div>
+              <ul className="pa0 flex justify-around list">
 
-          <li>
-            <a rel='noopener noreferrer' target='_blank' className='link' href="http://shop.pareiovision.com">Shop</a>
-          </li>
-          <li>
-            <Link to='/' className="ml2 link">Listen</Link>
-          </li>
-          <li>
-            <Link to='/contact' className="ml2 link">Contact</Link>
-          </li>
-          </ul>
-      </nav>
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/contact" component={ContactPage}/>
-    </div>
+                <li>
+                  <a rel='noopener noreferrer' target='_blank' className='link' href="http://shop.pareiovision.com">Shop</a>
+                </li>
+                <li>
+                  <Link to='/' className="ml2 link">Listen</Link>
+                </li>
+                <li>
+                  <Link to='/contact' className="ml2 link">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+            <Route exact path="/" component={HomePage}/>
+            <Route path="/contact" component={ContactPage}/>
+          </div>
 
-    <Route/>
-  </Router>
-  )
+          <Route/>
+        </Router>
+
+    )
+  }
 }
 
-export default App;
+
