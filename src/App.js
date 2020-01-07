b6e1cf5b03284776bcb03a276ebf33a2fda8ec0c
Gideon Ibemere <gideonibemere@Gideon-Ibemere.lan1>
2020-01-06 23:12:20 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import logo from './assets/Pareiovision.png'
 import './App.css';
 import { ContactPage, HomePage } from "./pages";
 
-<<<<<<< HEAD
 const App = () => {
   return (
   <Router>
@@ -32,39 +31,10 @@ const App = () => {
       <Route exact path="/" component={HomePage}/>
       <Route path="/contact" component={ContactPage}/>
     </div>
-=======
-export default class App extends Component {
-  render() {
-    return (
-        <Router>
-          <div className="sans-serif ma3">
-            <nav className='red flex justify-between items-center'>
-              <div className="link">
-                <Link to="/" className="link"><h2 className='f2 ma0'>Issac Lewis</h2></Link>
-              </div>
-              <ul className="pa0 flex justify-around list">
 
-                <li>
-                  <a rel='noopener noreferrer' target='_blank' className='link' href="http://shop.pareiovision.com">Shop</a>
-                </li>
-                <li>
-                  <Link to='/' className="ml2 link">Listen</Link>
-                </li>
-                <li>
-                  <Link to='/contact' className="ml2 link">Contact</Link>
-                </li>
-              </ul>
-            </nav>
-            <Route exact path="/" component={HomePage}/>
-            <Route path="/contact" component={ContactPage}/>
-          </div>
->>>>>>> 75e44ec1e5af552148b0cae08e0df47d617b8370
-
-          <Route/>
-        </Router>
-
-    )
-  }
+    <Route/>
+  </Router>
+  )
 }
 
-
+export default App;
