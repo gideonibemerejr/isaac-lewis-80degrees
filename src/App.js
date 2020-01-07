d0c64ff36211b8dd73e26f67b98f44f5f717b94b
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2020-01-07 12:16:02 -0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
-import React from 'react';
+
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import logo from './assets/Pareiovision.png'
 import './App.css';
 import { ContactPage, HomePage } from "./pages";
+import React, {Component} from 'react';
 
-const App = () => {
-  return (
-  <Router>
-      <nav className='flex-column-m flex justify-center justify-between-ns items-center mb2'>
-        {/*<div className="link flex justify-center items-center justify-between-ns">*/}
-
-          <div className="w-25-l w-75-m relative">
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <nav className='flex-column flex-l justify-center justify-between-l items-center items-start-l'>
+            {/*<div className="link flex justify-center items-center justify-between-ns">*/}
             <Link to="/" className="link dim">
-              <img className="relative" style={{"top": 0, "left": 0}} src={logo} alt=""/>
-              <img className="absolute" style={{"top": 0, "left": 0}} src={logo} alt=""/>
-              <img className="absolute" style={{"top": 0, "left": 0}} src={logo} alt=""/>
+              <div className=" ma0 w-25-ladd relative flex-column justify-start-l justify-center items-center">
+                <img src={logo} alt=""/>
+                <img src={logo} alt=""/>
+                <img src={logo} alt=""/>
+              {/*</div>*/}
+              </div>
             </Link>
-          {/*</div>*/}
-        </div>
-        <ul className="pa0 flex justify-around list mt4 mt0-l">
-          <li>
-            <a rel='noopener noreferrer' target='_blank' className='link white dim' href="http://shop.pareiovision.com">Shop</a>
-          </li>
-          <li>
-            <Link to='/' className="ml4 link white dim">Listen</Link>
-          </li>
-          <li>
-            <Link to='/contact' className="ml4 link white dim">Contact</Link>
-          </li>
-          </ul>
-      </nav>
-      <Route exact path="/" component={HomePage}/>
-      <Route path="/contact" component={ContactPage}/>
+            <ul className="pa0 flex justify-around list mt4 mt0-l">
+              <li>
+                <a rel='noopener noreferrer' target='_blank' className='link white dim' href="http://shop.pareiovision.com">Shop</a>
+              </li>
+              <li>
+                <Link to='/' className="ml4 link white dim">Listen</Link>
+              </li>
+              <li>
+                <Link to='/contact' className="ml4 link white dim">Contact</Link>
+              </li>
+            </ul>
+          </nav>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/contact" component={ContactPage}/>
 
 
-    <Route/>
-  </Router>
-  )
+          <Route/>
+        </Router>
+    );
+  }
 }
+
 export default App;
+
