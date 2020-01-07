ae73d2269b454c22736ebce3c822ed13a8ef7622
Gideon Ibemere Jr <gideon.ibemere.jr@gmail.com>
2020-01-07 12:01:18 -0600

    Update logo to prepare for animatino

--- src/App.js
+++ src/App.js
@@ -7,30 +7,32 @@ import { ContactPage, HomePage } from "./pages";
 const App = () => {
   return (
   <Router>
-    <div className="sans-serif bg-black">
-      <nav className='flex flex-column-m justify-center justify-between-ns items-center pa4'>
+      <nav className='flex-column-m flex justify-center justify-between-ns items-center mb2'>
         {/*<div className="link flex justify-center items-center justify-between-ns">*/}
-          <div className="w-25-l">
-            <Link to="/" className="link">
-              <img src={logo} alt=""/>
+
+          <div className="w-25-l w-75-m relative">
+            <Link to="/" className="link dim">
+              <img className="relative" style={{"top": 0, "left": 0}} src={logo} alt=""/>
+              <img className="absolute" style={{"top": 0, "left": 0}} src={logo} alt=""/>
+              <img className="absolute" style={{"top": 0, "left": 0}} src={logo} alt=""/>
             </Link>
           {/*</div>*/}
         </div>
-        <ul className="pa0 flex justify-around list">
+        <ul className="pa0 flex justify-around list mt4 mt0-l">
           <li>
-            <a rel='noopener noreferrer' target='_blank' className='link white' href="http://shop.pareiovision.com">Shop</a>
+            <a rel='noopener noreferrer' target='_blank' className='link white dim' href="http://shop.pareiovision.com">Shop</a>
           </li>
           <li>
-            <Link to='/' className="ml2 link white">Listen</Link>
+            <Link to='/' className="ml4 link white dim">Listen</Link>
           </li>
           <li>
-            <Link to='/contact' className="ml2 link white">Contac TEst t</Link>
+            <Link to='/contact' className="ml4 link white dim">Contact</Link>
           </li>
           </ul>
       </nav>
       <Route exact path="/" component={HomePage}/>
       <Route path="/contact" component={ContactPage}/>
-    </div>
+
 
     <Route/>
   </Router>
