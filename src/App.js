20102f65541cb19ea4dbdf6fab078e046b1d604c
Gideon Ibemere <gideonibemere@Gideon-Ibemere.lan1>
2020-01-06 22:44:30 -0600

    Update App.js to include pareiovision logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import logo from './assets/Pareiovision.png'
 import './App.css';
 import { ContactPage, HomePage } from "./pages";
 
 const App = () => {
   return (
   <Router>
-    <div className="sans-serif ma3">
-      <nav className='red flex justify-between items-center'>
-        <div className="link">
-          <Link to="/" className="link"><h2 className='f2 ma0'>Issac Lewis</h2></Link>
+    <div className="sans-serif bg-black">
+      <nav className='flex-ns flex-column justify-center items-center pa2'>
+        <div className="link flex justify-center items-center">
+          <div className="w-75 ma3">
+            <Link to="/" className="link">
+              <img src={logo} alt=""/>
+            </Link>
+          </div>
         </div>
         <ul className="pa0 flex justify-around list">
-
           <li>
-            <a rel='noopener noreferrer' target='_blank' className='link' href="http://shop.pareiovision.com">Shop</a>
+            <a rel='noopener noreferrer' target='_blank' className='link white' href="http://shop.pareiovision.com">Shop</a>
           </li>
           <li>
-            <Link to='/' className="ml2 link">Listen</Link>
+            <Link to='/' className="ml2 link white">Listen</Link>
           </li>
           <li>
-            <Link to='/contact' className="ml2 link">Contact</Link>
+            <Link to='/contact' className="ml2 link white">Contact</Link>
           </li>
           </ul>
       </nav>
