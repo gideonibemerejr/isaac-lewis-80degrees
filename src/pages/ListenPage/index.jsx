224c415d10cd317b9a8f7dae56adf904fd72418f
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-01-08 20:27:14 -0600

    Update albums

--- src/pages/ListenPage/index.jsx
+++ src/pages/ListenPage/index.jsx
@@ -1,80 +1,16 @@
 import React from "react";
-
-
-
+import './ListenPage.css'
 
 const ListenPage = () => {
     return (
-        <article className='flex flex-column justify-center items-center vh-75-l w-100'>
-            <inner className='flex flex-column justify-center items w-75'>
-                <div className='pa2 ml4-ns ml0 tc tl-ns'>
-                    <h2 className='f1'>Listen</h2>
-                </div>
-                <div className="cf pa2 ">
-                    <div className="fl w-100 w-25-m w-20-l ma4-ns ma0-ns">
-                        <a rel='noopener noreferrer'
-                           target='_blank' href="https://lnkfi.re/7WnC3Ac5"
-                           className="db link dim tc">
-                            <img
-                                src="https://s.mxmcdn.net/images-storage/albums4/4/6/0/3/2/8/42823064_350_350.jpg"
-                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
-                            <dl className="mt2 f6 lh-copy">
-                                <dt className="clip">Title</dt>
-                                <dd className="ml0 white truncate w-100">Dim</dd>
-                                <dt className="clip">Artist</dt>
-                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
-                            </dl>
-                        </a>
-                    </div>
-                    <div className="fl w-100 w-25-m w-20-l ma4-ns ma0">
-                        <a rel='noopener noreferrer'
-                           target='_blank'
-                           href="https://lnkfi.re/6YXt6bcI"
-                           className="db link dim tc">
-                            <img
-                                src="https://s.mxmcdn.net/images-storage/albums4/0/2/6/7/2/7/37727620_350_350.jpg"
-                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
-                            <dl className="mt2 f6 lh-copy">
-                                <dt className="clip">Title</dt>
-                                <dd className="ml0 white truncate w-100">Fly</dd>
-                                <dt className="clip">Artist</dt>
-                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
-                            </dl>
-                        </a>
-                    </div>
-                    <div className="fl w-100 w-25-m w-20-l ma4-ns ma0">
-                        <a rel='noopener noreferrer'
-                           target='_blank'
-                            href="https://lnkfi.re/rWAOPbGC"
-                           className="db link dim tc">
-                            <img
-                                src="https://is5-ssl.mzstatic.com/image/thumb/Music128/v4/c6/e9/79/c6e9799f-cf1a-4926-afd3-42899fa6717d/artwork.jpg/804x0w.jpg"
-                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
-                            <dl className="mt2 f6 lh-copy">
-                                <dt className="clip">Title</dt>
-                                <dd className="ml0 white truncate w-100">Pareidolia</dd>
-                                <dt className="clip">Artist</dt>
-                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
-                            </dl>
-                        </a>
-                    </div>
-                    <div className="fl w-100 w-25-m w-20-l ma4-ns ma0">
-                        <a rel='noopener noreferrer'
-                           target='_blank'
-                            href="https://lnkfi.re/PR199bUe"
-                           className="db link dim tc">
-                            <img
-                                src="https://is5-ssl.mzstatic.com/image/thumb/Music128/v4/c6/e9/79/c6e9799f-cf1a-4926-afd3-42899fa6717d/artwork.jpg/804x0w.jpg"
-                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
-                            <dl className="mt2 f6 lh-copy">
-                                <dt className="clip">Title</dt>
-                                <dd className="ml0 white truncate w-100">Pareidolia</dd>
-                                <dt className="clip">Artist</dt>
-                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
-                            </dl>
-                        </a>
-                    </div>
-                    <div className="fl w-100 w-25-m w-20-l ma4-ns ma0 ma0">
+        <article className='flex flex-column justify-center h-100 w-100 mt4'>
+
+
+
+            <div className='w-100 albums flex flex-column justify-center'>
+                <h2 className='f1'>Listen</h2>
+                <div className="flex flex-wrap cf">
+                    <div className="fl w-100  ma2 album">
                         <a rel='noopener noreferrer'
                            target='_blank'
                            href="https://ditto.fm/80-degrees-velvet"
@@ -90,13 +26,79 @@ const ListenPage = () => {
                             </dl>
                         </a>
                     </div>
+                    <div className="fl w-100   ma2  album ">
+                        <a rel='noopener noreferrer'
+                           target='_blank'
+                           href="https://lnkfi.re/PR199bUe"
+                           className="db link dim tc">
+                            <img
+                                src="https://m.media-amazon.com/images/I/81AQe4XHY9L._SS500_.jpg"
+                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 white truncate w-100">Silk</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
+                            </dl>
+                        </a>
+                    </div>
+                    <div className="fl w-100   ma2  album ">
+                        <a rel='noopener noreferrer'
+                           target='_blank'
+                           href="https://lnkfi.re/6YXt6bcI"
+                           className="db link dim tc">
+                            <img
+                                src="https://s.mxmcdn.net/images-storage/albums4/0/2/6/7/2/7/37727620_350_350.jpg"
+                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 white truncate w-100">Fly</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
+                            </dl>
+                        </a>
+                    </div>
+
+                    <div className="fl w-100   ma2  album ">
+                        <a rel='noopener noreferrer'
+                           target='_blank'
+                           href="https://lnkfi.re/rWAOPbGC"
+                           className="db link dim tc">
+                            <img
+                                src="https://is5-ssl.mzstatic.com/image/thumb/Music128/v4/c6/e9/79/c6e9799f-cf1a-4926-afd3-42899fa6717d/artwork.jpg/804x0w.jpg"
+                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 white truncate w-100">Pareidolia</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
+                            </dl>
+                        </a>
+                    </div>
+
+                    <div className="fl w-100   ma2  album ">
+                        <a rel='noopener noreferrer'
+                           target='_blank'href="https://lnkfi.re/7WnC3Ac5"
+                           className="db link dim tc">
+                            <img
+                                src="https://s.mxmcdn.net/images-storage/albums4/4/6/0/3/2/8/42823064_350_350.jpg"
+                                alt="Frank Ocean Blonde Album Cover" className="w-100 db outline black-10"/>
+                            <dl className="mt2 f6 lh-copy">
+                                <dt className="clip">Title</dt>
+                                <dd className="ml0 white truncate w-100">Dim</dd>
+                                <dt className="clip">Artist</dt>
+                                <dd className="ml0 gray truncate w-100">Isaac Lewis</dd>
+                            </dl>
+                        </a>
+                    </div>
+
                 </div>
 
-            </inner>
+            </div>
 
 
         </article>
     )
-};
+}
 
 export default ListenPage
