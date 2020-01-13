1828797bcd9c31727842888efef984f1b517e4c6
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2020-01-13 13:21:36 -0600

    Update

---
+++ isaac-lewis-80degrees/build/precache-manifest.baa516caee36fc974b7ff06c3367d6e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f8b3b9c1a3e587f34a08ed4e4806b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1e9f9d52104349e66fc",
+    "url": "/static/css/2.77d96167.chunk.css"
+  },
+  {
+    "revision": "83fe93965cb47831a427",
+    "url": "/static/css/main.8a8c9978.chunk.css"
+  },
+  {
+    "revision": "d1e9f9d52104349e66fc",
+    "url": "/static/js/2.71dae0d8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.71dae0d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83fe93965cb47831a427",
+    "url": "/static/js/main.aadb3143.chunk.js"
+  },
+  {
+    "revision": "7b11f9b887cde114bb5b",
+    "url": "/static/js/runtime-main.67d287d1.js"
+  },
+  {
+    "revision": "e6c10f92af6bd9f2942bff639f0c3666",
+    "url": "/static/media/80DegreesVelvertCoverARtfinal.e6c10f92.jpg"
+  },
+  {
+    "revision": "d3273b870d0d526b72199247b1b3868f",
+    "url": "/static/media/Pareiovision.d3273b87.png"
+  }
+]);
